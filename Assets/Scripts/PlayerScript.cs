d5416baae143e5195d02a2ba89f2c08f1f8eba22
rabbet35 <rabbet35@hotmail.com>
2020-11-23 13:17:10 +1100

    Updated end trigger, and added scene reload on contact with player.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Finish")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/Scripts/lavaScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class lavaScript : MonoBehaviour
+{
+    public Transform lavaTransform;
+    public Collider lavaCol;
+    public Collider playerCol;
+    public float startHeight = -10.0F;
+    public float moveSpeed = 0.01F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lavaTransform.position = new Vector3(0, startHeight, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 h = lavaTransform.position;
+        h.y += moveSpeed;
+        lavaTransform.position = h;
+    }
+}
